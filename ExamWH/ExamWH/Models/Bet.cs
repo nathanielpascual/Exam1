f6e5fb5cdf1b8f132b8d8b282b39c36aa6773130
nathanielpascual <nath_pascual@yahoo.com>
2016-09-14 23:35:06 +0800

    1

---
+++ ExamWH/ExamWH/Models/Bet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExamWH.Models
+{
+    public class Bet
+    {
+        public int Customer { get; set; }
+
+        public int Event { get; set; }
+
+        public int Participant { get; set; }
+
+        public float Stake { get; set; }
+
+        public float ToWin { get; set; }
+
+        public float Won { get; set; }
+
+        public bool hasWon { get; set; }
+    }
+}
---
+++ ExamWH/ExamWH/Models/BetList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExamWH.Models
+{
+    public class BetList
+    {
+        public BetList() {
+            Settled = new List<Bet>();
+            Unsettled = new List<Bet>();
+        }
+        public List<Bet> Settled { get; set; }
+
+        public List<Bet> Unsettled { get; set; }
+
+
+    }
+}
